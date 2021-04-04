1c1150f38cda25c7159eb9948dfd1f8f5afd6d59
Ulugbek <abdurahmons100@gmail.com>
2021-04-04 10:03:51 +0500

    Начальная версия

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ulugbek999/bank1
+
+go 1.16
---
+++ pkg/types/types.go
@@ -0,0 +1,57 @@
+package types
+
+
+
+//Money - представялет собой денежную сумму в минимальных единицах (центы, копейки, дирамы и т.д)
+type Money int64
+
+// Currency - представляет собой код валюты
+type Currency string
+
+
+// Коды валют
+
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+
+)
+
+// PAN - представялет номер карты
+type PAN string
+
+
+type Card struct {
+	ID int
+	PAN PAN
+	Balance Money
+	Currency Currency
+	Color string
+	Name string
+	Active bool
+}
+
+
+
+
+
+
+type PaymentSource struct {
+	Type string // 'card'
+	Number string // номер вида '5058 xxxx xxxx 8888'
+	Balance Money // баланс в дирамах
+	
+   }
+
+
+
+// Category представляет собой катенорию в которой был совершён платёж
+type Category string
+
+// Payment представляет информацию о платежах
+type Payment struct{
+	ID int
+	Amount Money
+	Category Category
+}
